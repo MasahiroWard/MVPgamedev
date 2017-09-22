201e826b90c08f8e1dfe89d6df59b346adf9366e
Masahiro Ward <mhward@utexas.edu>
2017-09-22 18:20:53 -0500

    Compartamentalize chameleon movement and inventory creation functions

--- Game/chameleon.js
+++ Game/chameleon.js
@@ -2,4 +2,26 @@
 
 // Put this in create
 
+function createChameleon(xcoor, ycoor){
+    player = game.add.sprite(xcoor,ycoor,'chameleon');
+    player.scale.setTo(0.1,0.1)
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 300;
+}
+
 // Put this in update
+
+function chameleonmove(){
+    game.physics.arcade.overlap(player, map_fruit, getfruit, null, this)
+        
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -300;
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 300;
+    } else if (cursors.up.isDown) {
+        player.body.velocity.y = -300;
+    }   
+}
