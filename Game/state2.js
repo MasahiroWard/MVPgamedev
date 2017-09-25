9b073c3374e16efe684b9d218c673a6acdccacf1
Vaidehi Narayan <vaidehi.narayan@utexas.edu>
2017-09-25 17:56:33 -0500

    test tile maps, they work :)

--- Game/state2.js
+++ Game/state2.js
@@ -35,10 +35,12 @@
 demo.state2 = function(){};
 demo.state2.prototype = {
     preload: function(){
-        
-        game.load.tilemap('field', 'assets/tilemaps/field.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('grassTiles', 'assets/tilemaps/grassTiles.png');
-        game.load.image('rockTiles', 'assets/tilemaps/rockTiles.png');
+        game.world.setBounds(0, 0, 2000, 1600);
+        game.load.tilemap('field', 'assets/tilemaps/TestTileMap6.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('UglySky', 'assets/tilemaps/UglySky.png');
+        game.load.image('UglyGrassTile', 'assets/tilemaps/UglyGrassTile.png');
+        game.load.image('UglyLadder', 'assets/tilemaps/UglyLadder.png');
+        //game.load.image('UglySky', 'assets/tilemaps/UglySky.png');
         
 //        game.load.tilemap('test', 'assets/tilemaps/TestTileMap.json', null, Phaser.Tilemap.TILED_JSON);
 //        game.load.image('UglyGrassTile', 'assets/tilemaps/UglyGrassTile.png');
@@ -52,16 +54,21 @@ demo.state2.prototype = {
     create: function(){
         
         
-        
+        game.camera.y = 1600;
         game.stage.backgroundColor = '#DDDDDD';
         addChangeStateEventListeners();
 
         var map = game.add.tilemap('field');
-        map.addTilesetImage('grassTiles');
-        map.addTilesetImage('rockTiles');
+        map.addTilesetImage('UglySky');
+        map.addTilesetImage('UglyGrassTile');
+        map.addTilesetImage('UglyLadder');
+       // map.addTilesetImage('UglySky');
 
-        grass = map.createLayer('grass');
-        rocks = map.createLayer('rocks');
+
+//        var ladder = map.createLayer('ladders');
+        var sky = map.createLayer('sky');
+        var ladders = map.createLayer('ladders');
+
 //
         
 //
@@ -76,6 +83,7 @@ demo.state2.prototype = {
        
     },
     update: function(){
+        game.camera.y -= camSpeed;
 
     }
 };
