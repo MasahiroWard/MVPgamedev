12c2d3e616280188cffb98c29f4fdcaaef1faafa
Vaidehi Narayan <vaidehi.narayan@utexas.edu>
2017-09-27 16:34:56 -0500

    bigger tile maps

--- Game/state3.js
+++ Game/state3.js
@@ -2,11 +2,11 @@ demo.state3 = function(){};
 demo.state3.prototype = {
     preload: function(){
         game.world.setBounds(0, 0, 2000, 3000);
-        game.load.tilemap('field', 'assets/tilemaps/TestTileMap6.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('UglySky', 'assets/tilemaps/UglySky.png');
-        game.load.image('UglyGrassTile', 'assets/tilemaps/UglyGrassTile.png');
-        game.load.image('UglyLadder', 'assets/tilemaps/UglyLadder.png');
-        //game.load.image('UglySky', 'assets/tilemaps/UglySky.png');
+        game.load.tilemap('stage', 'assets/tilemaps/TileMapBig.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('grass', 'assets/tilemaps/LargeGrass.png');
+//        game.load.image('UglyGrassTile', 'assets/tilemaps/UglyGrassTile.png');
+//        game.load.image('UglyLadder', 'assets/tilemaps/UglyLadder.png');
+//        //game.load.image('UglySky', 'assets/tilemaps/UglySky.png');
         
 //        game.load.tilemap('test', 'assets/tilemaps/TestTileMap.json', null, Phaser.Tilemap.TILED_JSON);
 //        game.load.image('UglyGrassTile', 'assets/tilemaps/UglyGrassTile.png');
@@ -24,16 +24,16 @@ demo.state3.prototype = {
         game.stage.backgroundColor = '#DDDDDD';
         addChangeStateEventListeners();
 
-        var map = game.add.tilemap('field');
-        map.addTilesetImage('UglySky');
-        map.addTilesetImage('UglyGrassTile');
-        map.addTilesetImage('UglyLadder');
+        var map = game.add.tilemap('stage');
+        map.addTilesetImage('grass');
+//        map.addTilesetImage('UglyGrassTile');
+//        map.addTilesetImage('UglyLadder');
        // map.addTilesetImage('UglySky');
 
 
 //        var ladder = map.createLayer('ladders');
-        var sky = map.createLayer('sky');
-        var ladders = map.createLayer('ladders');
+        var ledges = map.createLayer('Platforms');
+        var ladders = map.createLayer('Ladders');
 
 //
         
