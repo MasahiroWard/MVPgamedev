395d85641f4e5f4f0860d25555206810a8f0c22b
Masahiro Ward <mhward@utexas.edu>
2017-09-29 12:57:31 -0500

    Added the rest of the sprites

--- Game/loadImagesPre.js
+++ Game/loadImagesPre.js
@@ -14,13 +14,16 @@ function loadImages(){
     game.load.spritesheet('green_chameleon', 'assets/spritesheets/green_chameleon_spritesheet.png', 373, 258);
     game.load.spritesheet('blue_chameleon', 'assets/spritesheets/blue_chameleon_spritesheet.png', 373, 258);
     game.load.spritesheet('yellow_chameleon', 'assets/spritesheets/yellow_chameleon_spritesheet.png', 373, 259);
-    game.load.spritesheet('purple_chameleon', 'assets/spritesheets/purple_chameleon_spritesheet.png', 376, 259);    
+    game.load.spritesheet('purple_chameleon', 'assets/spritesheets/purple_chameleon_spritesheet.png', 376, 259);
+    game.load.spritesheet('red_chameleon', 'assets/spritesheets/red_chameleon_spritesheet.png', 373, 259);
+    game.load.spritesheet('orange_chameleon', 'assets/spritesheets/orange_chameleon_spritesheet.png', 373, 258);
 
-
-
-
-    
-    game.load.spritesheet('red_bird', 'assets/spritesheets/red_bird_spritesheet.png', 250, 500);
+    game.load.spritesheet('red_bird', 'assets/spritesheets/red_m1_spritesheet.png', 276, 213);
+    game.load.spritesheet('blue_bird', 'assets/spritesheets/blue_m1_spritesheet.png', 276, 213);
+    game.load.spritesheet('yellow_bird', 'assets/spritesheets/yellow_m1_spritesheet.png', 276, 213);
+    game.load.spritesheet('green_bird', 'assets/spritesheets/green_m1_spritesheet.png', 276, 213);
+    game.load.spritesheet('orange_bird', 'assets/spritesheets/orange_m1_spritesheet.png', 276, 213);
+    game.load.spritesheet('purple_bird', 'assets/spritesheets/purple_m1_spritesheet.png', 276, 214);    
     
     game.load.spritesheet('blue_snake', 'assets/spritesheets/blue_snake_spritesheet.png');
 }
