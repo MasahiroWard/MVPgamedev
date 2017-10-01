de801c785572c134e658134a4bc7cfb68cc958fd
Masahiro Ward <mhward@utexas.edu>
2017-10-01 11:29:40 -0500

    centered gameover text

--- Game/gameover.js
+++ Game/gameover.js
@@ -8,8 +8,9 @@ demo.gameover.prototype = {
     create: function(){
         game.stage.backgroundColor = "#ffffff";
         game.physics.startSystem(Phaser.Physics.Arcade);
-        var gameovertext = game.add.text(500, 500, "Game Over");
-        //gameovertext.addColor("#ffffff")
+        var style = {boundsAlignH: "center", boundsAlignV: "middle" }
+        var gameovertext = game.add.text(0, 0, "Game Over", style);
+        gameovertext.setTextBounds(0, 0, 1000, 700);
     },
     update: function(){
         
