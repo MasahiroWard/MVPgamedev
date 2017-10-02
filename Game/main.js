6fadc026ebcde04dbad0efb11be3fcbf04b49970
Vaidehi Narayan <vaidehi.narayan@utexas.edu>
2017-10-02 16:42:01 -0500

    added icestate to main

--- Game/main.js
+++ Game/main.js
@@ -4,4 +4,5 @@ game.state.add('state0', demo.state0);
 game.state.add('state1', demo.state1);
 game.state.add('state2', demo.state2);
 game.state.add('state3', demo.state3);
+game.state.add('icestate', demo.icestate);
 game.state.start('state3');
