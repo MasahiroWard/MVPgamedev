a70cbb198bf7c987bc5fbb5ebb3cbc150af98dda
Masahiro Ward <mhward@utexas.edu>
2017-10-05 17:34:01 -0500

    Yarn ball is working better now. Also renamed states

--- Game/statechanger.js
+++ Game/statechanger.js
@@ -1,21 +1,7 @@
 // Functions to change states.  These will have to be changed for the final product
 
-function changeState(i, stateNum){
-    console.log('state' + stateNum);
-    game.state.start('state' + stateNum);
-}
-
-function addKeyCallback(key, fn, args){
-    game.input.keyboard.addKey(key).onDown.add(fn, null, null, args);
-    game.input.keyboard.addCallbacks(this, null, null, keyPress);
-}
-
 function addChangeStateEventListeners(){
-    addKeyCallback(Phaser.Keyboard.ZERO, changeState, 0);
-    addKeyCallback(Phaser.Keyboard.ONE, changeState, 1);
-    addKeyCallback(Phaser.Keyboard.TWO, changeState, 2);
-    addKeyCallback(Phaser.Keyboard.THREE, changeState, 3);
-//    addKeyCallback(Phaser.Keyboard.FOUR, game.state.start('icestate'));
+    game.input.keyboard.addCallbacks(this, null, null, keyPress);
 }
 
 function keyPress(char) {
