b8739a72bf09b3bff84a458f3ad53438176b3e0b
Vaidehi Narayan <vaidehi.narayan@utexas.edu>
2017-11-14 14:15:50 -0600

    changed bestiary alignment

--- Game/bestiary.js
+++ Game/bestiary.js
@@ -32,35 +32,62 @@ demo.bestiary.prototype = {
 
         
         // Add chameleon at x,y
-        createChameleon(500,game.world.height - 200);
+//        createChameleon(500,game.world.height - 200);
         
+        
+        var bestiary_option = game.add.sprite(500, 50, "bestiary_txt");
+        bestiary_option.anchor.setTo(0.5,0.5);
+        
+        var style = {font: "30px Arial", fill: "Black"}
+        var main_menu_option = game.add.sprite(500, 550, "main_menu_txt");
+        main_menu_option.anchor.setTo(0.5, 0.5);
+        main_menu_option.inputEnabled = true;
+        main_menu_option.events.onInputUp.add(
+            function(){
+                game.state.start('menu');
+            }
+        );
+        main_menu_option.events.onInputOver.add(
+            function() {
+                main_menu_option.scale.setTo(1.2, 1.2);
+            }
+        );
+        main_menu_option.events.onInputOut.add(
+            function () {
+                main_menu_option.scale.setTo(1, 1);
+            }
+        );
+
+
+        var move = 50;
+        var movex = 150;
         // make groups
         make_fruit_groups();
         make_enemy_groups();
         make_balloon_group();
-        addMovingPlatforms();
-        make_healthpack_groups();
+//        addMovingPlatforms();
+//        make_healthpack_groups();
         
-        placeBird(100, 100, ["red"])
-        placeBird(200, 100, ["orange"])
-        placeBird(300, 100, ["yellow"])
-        placeBird(400, 100, ["green"])
-        placeBird(500, 100, ["blue"])
-        placeBird(600, 100, ["purple"])
+        placeBird(100 + movex, 100+move, ["red"])
+        placeBird(200+ movex, 100+move, ["orange"])
+        placeBird(300+ movex, 100+move, ["yellow"])
+        placeBird(400+ movex, 100+move, ["green"])
+        placeBird(500+ movex, 100+move, ["blue"])
+        placeBird(600+ movex, 100+move, ["purple"])
         
-        placeSnake(100, 200, ["red"])
-        placeSnake(200, 200, ["orange"])
-        placeSnake(300, 200, ["yellow"])
-        placeSnake(400, 200, ["green"])
-        placeSnake(500, 200, ["blue"])
-        placeSnake(600, 200, ["purple"])
+        placeSnake(100+ movex, 200+move, ["red"])
+        placeSnake(200+ movex, 200+move, ["orange"])
+        placeSnake(300+ movex, 200+move, ["yellow"])
+        placeSnake(400+ movex, 200+move, ["green"])
+        placeSnake(500+ movex, 200+move, ["blue"])
+        placeSnake(600+ movex, 200+move, ["purple"])
         
-        c1 = game.add.sprite(100, 350, 'red_chameleon')
-        c2 = game.add.sprite(200, 350, 'orange_chameleon')
-        c3 = game.add.sprite(300, 350, 'yellow_chameleon')
-        c4 = game.add.sprite(400, 350, 'green_chameleon')
-        c5 = game.add.sprite(500, 350, 'blue_chameleon')
-        c6 = game.add.sprite(600, 350, 'purple_chameleon')
+        c1 = game.add.sprite(100+ movex, 350+move, 'red_chameleon')
+        c2 = game.add.sprite(200+ movex, 350+move, 'orange_chameleon')
+        c3 = game.add.sprite(300+ movex, 350+move, 'yellow_chameleon')
+        c4 = game.add.sprite(400+ movex, 350+move, 'green_chameleon')
+        c5 = game.add.sprite(500+ movex, 350+move, 'blue_chameleon')
+        c6 = game.add.sprite(600+ movex, 350+move, 'purple_chameleon')
         
         c1.anchor.setTo(0.5,0.5)
         c2.anchor.setTo(0.5,0.5)
@@ -76,19 +103,19 @@ demo.bestiary.prototype = {
         c5.scale.setTo(0.13, 0.13)
         c6.scale.setTo(0.13, 0.13)
         
-        placeFruit(100 - 25, 250, "redfruit")
-        placeFruit(200 - 25, 250, "orangefruit")
-        placeFruit(300 - 25, 250, "yellowfruit")
-        placeFruit(400 - 25, 250, "greenfruit")
-        placeFruit(500 - 25, 250, "bluefruit")
-        placeFruit(600 - 25, 250, "purplefruit")
+        placeFruit(100 - 25+ movex, 250+move, "redfruit")
+        placeFruit(200 - 25+ movex, 250+move, "orangefruit")
+        placeFruit(300 - 25+ movex, 250+move, "yellowfruit")
+        placeFruit(400 - 25+ movex, 250+move, "greenfruit")
+        placeFruit(500 - 25+ movex, 250+move, "bluefruit")
+        placeFruit(600 - 25+ movex, 250+move, "purplefruit")
         
-        t1 = game.add.sprite(100, 400, 'rtile')
-        t2 = game.add.sprite(200, 400, 'otile')
-        t3 = game.add.sprite(300, 400, 'ytile')
-        t4 = game.add.sprite(400, 400, 'gtile')
-        t5 = game.add.sprite(500, 400, 'btile')
-        t6 = game.add.sprite(600, 400, 'ptile')
+        t1 = game.add.sprite(100+ movex, 400+move, 'rtile')
+        t2 = game.add.sprite(200+ movex, 400+move, 'otile')
+        t3 = game.add.sprite(300+ movex, 400+move, 'ytile')
+        t4 = game.add.sprite(400+ movex, 400+move, 'gtile')
+        t5 = game.add.sprite(500+ movex, 400+move, 'btile')
+        t6 = game.add.sprite(600+ movex, 400+move, 'ptile')
         
         t1.anchor.setTo(0.5,0.5)
         t2.anchor.setTo(0.5,0.5)
@@ -104,28 +131,28 @@ demo.bestiary.prototype = {
         t5.scale.setTo(0.6,0.6)
         t6.scale.setTo(0.6,0.6)
         
-        placeMP(0, 600, 20, 3, 0, 0, 0, 0);
+//        placeMP(0, 600, 20, 3, 0, 0, 0, 0);
 
 
             
         // These should be the last thing added so that it is on top of all other sprites (never hidden)
-        createInventory(0, 525);
-        place_hearts(450, 0);
+//        createInventory(0, 525);
+//        place_hearts(450, 0);
         add_pause_darkener();
         game.input.onDown.add(pause_clicking, self);
     },
     update: function(){
 
 
-        if (player.ballooning) {
-            chameleon_float();
-        } else {
-            chameleonmove();
-        }
+//        if (player.ballooning) {
+//            chameleon_float();
+//        } else {
+//            chameleonmove();
+//        }
         
         birds_group.forEach(moveBird, this);
         snakes_group.forEach(moveSnake, this);
-        moving_platform_group.forEach(movingPlatformsUpdate, this);
-        update_health(player.health);
+//        moving_platform_group.forEach(movingPlatformsUpdate, this);
+//        update_health(player.health);
     },
 }
